2da59f3dba449034f6af1ba2e54b409545c42d57
Anzhelika Shilyaeva <shilyaeva068@icloud.com>
2024-07-01 17:05:47 +0300

    add axios

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,28 +1,28 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import styles from './Filter.module.scss'
-import TextField from '@mui/material/TextField'
+// import TextField from '@mui/material/TextField'
 // import { TextMaskCustom } from './TextMaskCustom'
 
 const Filter = () => {
-	const genres = ['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi']
-	const [selectedGenres, setSelectedGenres] = useState([])
+	// const genres = ['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi']
+	// const [selectedGenres, setSelectedGenres] = useState([])
 
-	const handleCheckboxChange = genre => {
-		if (selectedGenres.includes(genre)) {
-			setSelectedGenres(
-				selectedGenres.filter(selectedGenre => selectedGenre !== genre),
-			)
-		} else {
-			setSelectedGenres([...selectedGenres, genre])
-		}
-	}
+	// const handleCheckboxChange = genre => {
+	// 	if (selectedGenres.includes(genre)) {
+	// 		setSelectedGenres(
+	// 			selectedGenres.filter(selectedGenre => selectedGenre !== genre),
+	// 		)
+	// 	} else {
+	// 		setSelectedGenres([...selectedGenres, genre])
+	// 	}
+	// }
 
-	const handleSearch = () => {
-		console.log('Выбранные жанры для поиска:', selectedGenres)
-	}
+	// const handleSearch = () => {
+	// 	console.log('Выбранные жанры для поиска:', selectedGenres)
+	// }
 	return (
 		<div className={styles.filter}>
-			<ul className={styles.filter__container}>
+			{/* <ul className={styles.filter__container}>
 				<li>
 					<h2 className={styles.filter__title}>По жанру</h2>
 					<ul className={styles.filter__block}>
@@ -67,18 +67,18 @@ const Filter = () => {
 						</li>
 					</ul>
 				</li>
-			</ul>
+			</ul> */}
 
-			<TextField
+			{/* <TextField
 				label='Введите год'
 				InputProps={{
 					inputComponent: TextMaskCustom,
 				}}
-			/>
+			/> */}
 
-			<button className={styles.filter__button} onClick={handleSearch}>
+			{/* <button className={styles.filter__button} onClick={handleSearch}>
 				Поиск
-			</button>
+			</button> */}
 		</div>
 	)
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
+import axios from 'axios'
 import { useEffect, useState } from 'react'
+import Filter from '../../components/Filter/Filter'
 import MoviesCardList from '../../components/MoviesCardList/MoviesCardList'
+import PaginationOutlined from '../../components/PaginationOutlined/PaginationOutlined'
 import { IResponse } from '../../interfaces/data'
 import styles from './Home.module.scss'
-import Filter from '../../components/Filter/Filter'
-import PaginationOutlined from '../../components/PaginationOutlined/PaginationOutlined'
 
 const BASE_URL = import.meta.env.VITE_BASE_URL
 const API_KEY = import.meta.env.VITE_API_KEY
@@ -12,34 +13,34 @@ const Home = () => {
 	const [movies, setMovies] = useState<IResponse | null>(null)
 
 	useEffect(() => {
+		async function fetchMovies() {
+			try {
+				const { data } = await axios.get(`${BASE_URL}`, {
+					method: 'GET',
+					headers: {
+						'X-API-KEY': `${API_KEY}`,
+					},
+				})
+				localStorage.setItem('moviesList', JSON.stringify(data))
+				setMovies(data)
+			} catch (e) {
+				console.log('Произошла ошибка, при получении фильмов.', e)
+			}
+		}
 		const storageMovies = localStorage.getItem('moviesList')
 
 		if (storageMovies !== null) {
 			const parseMovies: IResponse = JSON.parse(storageMovies)
 			setMovies(parseMovies)
 		} else {
-			console.log('tyt')
-			fetch(`${BASE_URL}`, {
-				method: 'GET',
-				headers: {
-					'X-API-KEY': `${API_KEY}`,
-				},
-			})
-				.then(res => res.json())
-				.then(data => {
-					localStorage.setItem('moviesList', JSON.stringify(data))
-					setMovies(data)
-				})
-				.catch(err => console.log('Ошибка:(', err))
+			fetchMovies()
 		}
 	}, [])
 
 	return (
 		<div className={styles.home}>
 			<Filter />
-			<main>
-				<MoviesCardList movies={movies} />
-			</main>
+			<main>{movies !== null && <MoviesCardList movies={movies} />}</main>
 			<PaginationOutlined />
 		</div>
 	)
